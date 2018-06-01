827980ae8a7a77820213247c39f0e0071b069da1
xtreak <tir.karthi@gmail.com>
2018-06-01 04:07:30 +0000

    Use regex for parsing

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 import json
 import os
+import re
 import time
 
 import praw
+from praw.exceptions import APIException
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
@@ -18,7 +20,7 @@ FOOTER_STRING = '''
 
 
 
-This bot was created by [xtreak](https://www.reddit.com/r/xtreak). Please contact the author for any bugs or raise issues in GitHub. The source code is at [GitHub](https://www.github.com/tirkarthi/stoicbot). Feedback is welcome.
+This bot was created by [xtreak](https://www.reddit.com/u/xtreak). Please contact the author for any bugs or raise issues in GitHub. The source code is at [GitHub](https://www.github.com/tirkarthi/stoicbot). Feedback is welcome.
 '''
 
 def main():
@@ -32,26 +34,32 @@ def main():
                          user_agent=user_agent)
 
     while True:
-        print("Waiting for a reply")
-        for comment in reddit.inbox.unread(limit=25):
-            print("Got mention from {0} with body {1}".format(comment.author, comment.body))
-            quote = None
-            body = comment.body
+        try:
+            for comment in reddit.inbox.unread(limit=25):
+                print("Got mention from {0} with body {1}".format(comment.author, comment.body))
+                quote = None
+                body = comment.body
 
-            if body:
-                mention, quote = body.split(' ')
-                quote = quote.strip()
-                quote = data.get(quote)
-                if quote:
-                    quote = quote + FOOTER_STRING
-                    comment.reply(quote)
-                    comment.mark_read()
+                match = re.search("u/stoicbot (\d+.\d+)", body)
+                if match:
+                    verse = match.group(1)
+                    quote = data.get(verse)
+                    if quote:
+                        quote = quote + FOOTER_STRING
+                        comment.reply(quote)
+                        comment.mark_read()
+                    else:
+                        comment.mark_read()
+                        comment.reply("No quote for the given verse.")
                 else:
                     comment.mark_read()
-                    print("No quotes for this. Please specify in the format 'chapter_number.verse_number'")
-            else:
-                comment.mark_read()
-                print("No quotes for this. Please specify in the format 'chapter_number.verse_number'")
+                    comment.reply("No quotes for this. Please specify in the format 'chapter_number.verse_number'")
+        except APIException as e:
+            print("API Exception occured : ", e)
+            time.sleep(700)
+        except Exception as e:
+            print("Exception occured : ", e)
+            time.sleep(10)
 
         # Don't hit reddit server for every second
         time.sleep(10)
